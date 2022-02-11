deda380ea804d863c5f4f3f0c600a9ade9dd9456
Samuel <deboadeabyo@yahoo.com>
2022-02-11 21:25:30 +0000

    commit on 'master'

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Random
+
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string name;
+            string city;
+            sbyte age;
+            int pin;
+            int house_number;
+ 
+            
+            Console.WriteLine("Enter your name\n");
+            name = Console.ReadLine();
+ 
+            Console.WriteLine("Enter Your City\n");
+            city = Console.ReadLine();
+ 
+            
+            Console.WriteLine("Enter your age\n");
+            age = sbyte.Parse(Console.ReadLine());
+
+            
+            
+ 
+            Console.WriteLine("Enter your pin\n");
+            pin = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your house number?\n");
+            house_number = Int32.Parse(Console.ReadLine());
+ 
+            
+            Console.WriteLine("==============");
+            Console.WriteLine("Your Complete Address:");
+            Console.WriteLine("============\n");
+ 
+            Console.WriteLine($"Name {name} ");
+            Console.WriteLine($"City {city} ");
+            Console.WriteLine($"Age {age}");
+            Console.WriteLine($"Pin  {pin}");
+            Console.WriteLine($"House Number {house_number} ");
+            Console.WriteLine("===============");
+ 
+            Console.ReadLine();
+
+        }
+    }
+}
